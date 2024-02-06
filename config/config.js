6a4bb19513dac841a8429d632a93e3f313d57910
rmac3392 <ryanjamesmacawili@gmail.com>
2024-02-06 12:40:04 +0800

    important revision

--- config/config.js
+++ config/config.js
@@ -1,14 +1,20 @@
-const {createConnection } = require('mysql');
+const { createConnection } = require("mysql");
+// const con = createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123",
+//   database: "megaland_db",
+// });
 const con = createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"megaland_db"
+  host: "srv545.hstgr.io",
+  user: "u484898470_megaland",
+  password: "Concentrix70139",
+  database: "u484898470_megaland_db",
 });
 
-con.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-  });
+con.connect((error) => {
+  if (error) throw error;
+  console.log("Successfully connected to the database.");
+});
 
 module.exports = con;
