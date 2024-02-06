7a90784440f07439f3fa52af72da49d5e6ef4a10
rmac3392 <ryanjamesmacawili@gmail.com>
2024-02-06 14:06:01 +0800

    pls run

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 const { createPool } = require("mysql");
 
-const con = createPool({
-  host: "localhost",
-  user: "root",
-  password: "123",
-  database: "megaland_db",
-});
-
-// const con = createConnection({
-//   host: "srv545.hstgr.io",
-//   user: "u484898470_megaland",
-//   password: "Concentrix70139",
-//   database: "u484898470_megaland_db",
+// const con = createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "123",
+//   database: "megaland_db",
 // });
+
+const con = createPool({
+  host: "srv545.hstgr.io",
+  user: "u484898470_megaland",
+  password: "Concentrix70139",
+  database: "u484898470_megaland_db",
+});
 
 // con.connect((error) => {s
 module.exports = con;
