de8781a54605862dbb99a872978806a2ca0a4de4
rmac3392 <ryanjamesmacawili@gmail.com>
2024-02-06 13:16:59 +0800

    update credentials

--- config/config.js
+++ config/config.js
@@ -1,20 +1,18 @@
-const { createConnection } = require("mysql");
+const { createPool } = require("mysql");
+
+const con = createPool({
+  host: "localhost",
+  user: "root",
+  password: "123",
+  database: "megaland_db",
+});
+
 // const con = createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "123",
-//   database: "megaland_db",
+//   host: "srv545.hstgr.io",
+//   user: "u484898470_megaland",
+//   password: "Concentrix70139",
+//   database: "u484898470_megaland_db",
 // });
-const con = createConnection({
-  host: "srv545.hstgr.io",
-  user: "u484898470_megaland",
-  password: "Concentrix70139",
-  database: "u484898470_megaland_db",
-});
 
-con.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
-
+// con.connect((error) => {s
 module.exports = con;
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-
-
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 var corsOptions = {
-    origin: "http://megaland.vercel.app"
+  origin: "http://megaland.vercel.app",
 };
 
 app.use(cors(corsOptions));
@@ -15,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.json({ message: "KONEK NAKAS PORT 8080" });
+  res.json({ message: "KONEK NAKAS PORT 8080" });
 });
 
 // Require and use the routes module, passing the app instance
@@ -24,5 +22,5 @@ require("./routes/megaland.routes.js")(app);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
